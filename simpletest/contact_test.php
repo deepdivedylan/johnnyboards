9277d63e500274a125f11fed08998b4f5a1a4f2b
Ruben Garcia <ruben@johnnyboards.com>
2014-06-26 12:03:16 -0600

    contact

---
+++ simpletest/contact_test.php
@@ -0,0 +1,120 @@
+<?php
+	// grab the unit test framework
+	require_once("/usr/lib/php5/simpletest/autorun.php");
+	
+	// grab the functions under scrutiny
+	require_once("../php/contact.php");
+        
+	// grab the config file
+	require_once("/home/bradg/tutorconnect/config.php");
+	
+	class ContactTest extends UnitTestCase
+	{
+		private $mysqli = null;
+            
+		// variable to hold the mysqli objects
+		private $sqlContact;
+		private $contact;
+            
+		// constant variables to reuse. set interger or string
+		 /*
+             *input: (integer) idcontact
+             *input: (string) company name
+             *input: (string) address line 1
+             *input: (string) address line 2
+             *input: (string) city
+             *input: (string) zipcode
+             *input: (string) state
+             *input: (string) phone
+             *input: (string) email */
+		 
+		//member variables
+	    private $id; // this is assigned as -1 
+            private $idcontact= 7;
+            private $companyName = "Flying Star";
+            private $address1="832 Madison NE";
+            private $address2= "n/a";
+            private $city="Albuquerque";
+            private $zipcode="87110";
+            private $state="NM";
+            private $phoneNumber="505-363-4106";
+            private $email="ruben@johnnyboards.com";	 
+			
+			
+		 
+		 // setup() is before *EACH test           
+		public function setUp()
+		{
+			try
+			{
+				if($this->mysqli === null)
+				{
+					$this->mysqli = Pointer::getMysqli();
+				}
+				$this->contact = new Contact (-1, $this->idcontact, $this->companyName,
+				$this->address1, $this->address2,$this->city);
+				$this->zipcode, $this->phoneNumber,$this->email);
+				$this->contact->insert($this->mysqli);
+			}
+			catch(mysqli_sql_exception $exception)
+			{
+				echo "Unable to connect to mySQL: " . $exception->getMessage();
+			}
+		}
+			//then assert, this was called ok() in qunit  
+		public function testGetContactsByIdContact()
+		{
+			$this->$sqlContact = Contact::getContactsByIdContact($this->mysqli, $this->idcontact);
+			$this->assertIdentical($this->contact, $this->$sqlContact[0]);
+		}
+			//setup your expectations
+		public function testGetContactsByIdContactInvalid()
+		{
+			$this->expectException("Exception");
+			@Contact::getContactsByIdContact($this->mysqli, 0);
+		}
+     
+		public function testValidUpdateContact()
+		{	
+			$newIdContact = "JB contacts are better";
+			$newCompanyName 		 = "Just ask me and I'll tell you.";
+			$newAddress1 = "Just ask me and I'll tell you.";
+			$newAddress2= "Just ask me and I'll tell you.";
+			$newCity= "Just ask me and I'll tell you.";
+			$newZipCode= "Just ask me and I'll tell you.";
+			$newPhoneNumber= "Just ask me and I'll tell you.";
+			$newEmail= "Just ask me and I'll tell you.";
+			
+			
+			$this->contact->setIdContact($newIdContact);
+			$this->contact->setCompanyName 		($newCompanyName 		);
+			$this->contact->setAddress1($newAddress1);
+			$this->contact->setAddress2($newAddress2);
+			$this->contact->setCity($newCity);
+			$this->contact->setZipCode($newZipCode);
+			$this->contact->setPhoneNumber($newPhoneNumber);
+			$this->contact->setEmail($newEmail);
+			$this->contact->update($this->mysqli);
+			
+											
+			//select the user from mySQL and assert it was inserted properly
+			$this->sqlContact = Contact::getContactByIdContact($this->mysqli, $this->idcontact);
+		
+			// verify the IdContact , CompanyName , Address1,Address2, City , zipCode, PhoneNumebr and Email changed
+			$this->assertIdentical($this->sqlContact[0]->getIdContact(), $newIdContact);
+			$this->assertIdentical($this->sqlContact[0]->getCompanyName (), $newCompanyName);
+			$this->assertIdentical($this->sqlContact[0]->getAdress1(), $newAddress1);
+			$this->assertIdentical($this->sqlContact[0]->getAddress2(), $newAddress2);
+			$this->assertIdentical($this->sqlContact[0]->getCity(), $newCity);
+			$this->assertIdentical($this->sqlContact[0]->getZipCode(), $newZipCode);
+			$this->assertIdentical($this->sqlContact[0]->getPhoneNumber(), $newPhoneNumber);
+			$this->assertIdentical($this->sqlContact[0]->getEmail(), $newEmail);
+            }
+            
+            // teardown
+            public function tearDown()
+            {
+                $this->contact->delete($this->mysqli);
+            }
+        }
+?>
